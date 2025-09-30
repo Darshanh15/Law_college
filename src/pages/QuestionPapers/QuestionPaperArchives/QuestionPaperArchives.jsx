3b37704b9469a23aa4cec645dcc397d4b186f38e
Darshanh15 <darshanhgowda5@gmail.com>
2025-09-30 16:22:12 +0530

    Fix routing and update app structure

--- src/pages/QuestionPapers/QuestionPaperArchives/QuestionPaperArchives.jsx
+++ src/pages/QuestionPapers/QuestionPaperArchives/QuestionPaperArchives.jsx
@@ -1,18 +1,60 @@
-    import React from "react";
+//     import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./QuestionPaperArchives.scss"
+// import s1 from "../../../assets/Pdf/sylobus/3y.pdf"
+// import s2 from "../../../assets/Pdf/sylobus/5y.pdf"
+
+// const subjects = [
+//   {
+//     // name: "Subject 1",
+//     fileId: "1hV5KWliI4odgfY4keUWQ0vA4PynVNZ2A",
+//   },
+//   {
+//     // name: "Subject 2",
+//     fileId: "1joUd5r1H1c5QitMoCgqepfHI95XZVV8J",
+//   }
+// ];
+
+// const QuestionPaperArchives = () => {
+//   return (
+//     <div className="container-fluid">
+//       <h3 className="text-center my-4">LIST OF SYLLABUS</h3>
+//       <div className="row">
+//         {subjects.map((subject, index) => (
+//           <div className="col-lg-6 mb-4" key={index}>
+//             <h5 className="text-center">{subject.name}</h5>
+//             <iframe
+//               src={`https://drive.google.com/file/d/${subject.fileId}/preview`}
+//               className="w-100"
+//               style={{ height: "90vh", border: "none" }}
+//               title={subject.name}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default QuestionPaperArchives;
+
+
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./QuestionPaperArchives.scss"
-import s1 from "../../../assets/Pdf/sylobus/3y.pdf"
-import s2 from "../../../assets/Pdf/sylobus/5y.pdf"
+import "./QuestionPaperArchives.scss";
+
+import s1 from "../../../assets/Pdf/sylobus/3y.pdf";
+import s2 from "../../../assets/Pdf/sylobus/5y.pdf";
 
 const subjects = [
   {
-    // name: "Subject 1",
-    fileId: "1hV5KWliI4odgfY4keUWQ0vA4PynVNZ2A",
+    name: "3 Year LLB Syllabus",
+    fileId: s1,
   },
   {
-    // name: "Subject 2",
-    fileId: "1joUd5r1H1c5QitMoCgqepfHI95XZVV8J",
-  }
+    name: "5 Year LLB Syllabus",
+    fileId: s2,
+  },
 ];
 
 const QuestionPaperArchives = () => {
@@ -22,9 +64,9 @@ const QuestionPaperArchives = () => {
       <div className="row">
         {subjects.map((subject, index) => (
           <div className="col-lg-6 mb-4" key={index}>
-            <h5 className="text-center">{subject.name}</h5>
+            <h5 className="text-center mb-3">{subject.name}</h5>
             <iframe
-              src={`https://drive.google.com/file/d/${subject.fileId}/preview`}
+              src={subject.fileId}  
               className="w-100"
               style={{ height: "90vh", border: "none" }}
               title={subject.name}
